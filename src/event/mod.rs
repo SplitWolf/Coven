cf56238fd8c02c701b3f93fbd88b25cfda7bf17a
SplitWolf <mat.jake1360@gmail.com>
2023-12-30 03:33:25 -0500

    feat(Events): Add event dispatcher

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -2,6 +2,8 @@
 pub mod key_event;
 pub mod mouse_event;
 pub mod application_event;
+
+#[derive(PartialEq)]
 pub enum EventType {
     WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
     AppTick, AppUpdate, AppRender,
@@ -17,6 +19,9 @@ macro_rules! BIT {
 #[macro_export]
 macro_rules! EVENT_STRUCT_TYPE {
     ($x:tt) => {
+        fn get_static_type() -> super::EventType {
+            super::EventType::$x
+        }
         fn get_event_type(&self) -> super::EventType {
             super::EventType::$x
         }
@@ -46,6 +51,7 @@ pub trait Event {
     fn is_in_category(&self, category: EventCategory) -> bool {
         self.get_category_flags() & category as i32 != 0
     } 
+    fn get_static_type() -> EventType where Self: Sized;
     fn get_event_type(&self) -> EventType;
     fn get_category_flags(&self) -> i32;
     fn get_name(&self) -> &str;
@@ -56,5 +62,25 @@ pub trait Event {
 impl std::fmt::Display for dyn Event {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.get_name())
+    }
+}
+
+pub struct EventDispatcher<'eventlife> {
+    event: &'eventlife mut dyn Event
+}
+
+impl EventDispatcher<'_> {
+    
+    pub fn new(event: &mut dyn Event) -> EventDispatcher {
+        EventDispatcher { event }
+    }
+    
+    
+    pub fn dispatch<Q: Event>(&mut self, func: impl Fn(&dyn Event) -> bool) -> bool {
+        if self.event.get_event_type() == Q::get_static_type() {
+            self.event.set_handled(func(self.event));
+            return true;
+        }
+        false
     }
 }
